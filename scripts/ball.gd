27bb32fa6ea28d4b5806ed72dda532bd108fb0fd
Miguel <martinezpmg9@gmail.com>
2024-12-20 20:53:24 +0100

    little enhancement over bounce

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -9,15 +9,26 @@ func _ready():
 
 func _physics_process(delta):
 	# Move the ball
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		# Reflect the velocity upon collision
-		velocity = velocity.bounce(collision.get_normal())
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	
+	if collision == null:
+		return
+	
+	var collider = collision.get_collider()
+	var to: Vector2 = collision.get_normal()
+	
+	if collider.name == "platform":
+		to = (position - collider.position).normalized()
+		to = to.from_angle(PI/2)
+		
+	# Reflect the velocity upon collision
+	velocity = velocity.bounce(to)
 
 	# If you want to adjust velocity mid-game:
 	#if Input.is_action_just_pressed("restart_ball"):
 		#reset_ball()
-
+	pass
+	
 func reset_ball():
 	global_position = Vector2(400, 300)  # Replace with your starting position
 	velocity = Vector2(initial_speed, initial_speed).rotated(randf() * TAU)
